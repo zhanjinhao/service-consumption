e465f3647672fea126f8bd134d18a85706a55e4e
isjinhao <isjinhao@163.com>
2022-04-06 20:16:10 +0800

    add propertyrefresh test for business easy.

--- nacos-restful-provider/src/main/java/cn/isjinhao/sc/NacosRestfulProviderApplication.java
+++ nacos-restful-provider/src/main/java/cn/isjinhao/sc/NacosRestfulProviderApplication.java
@@ -1,13 +1,17 @@
 package cn.isjinhao.sc;
 
+import cn.addenda.businesseasy.propertyrefresh.EnablePropertyRefresh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @Author ISJINHAO
  * @Date 2021/4/18 19:08
  */
+@EnablePropertyRefresh(threadSizes = 2)
 @SpringBootApplication
+@EnableTransactionManagement
 public class NacosRestfulProviderApplication {
 
     public static void main(String[] args) {
--- nacos-restful-provider/src/main/java/cn/isjinhao/sc/provider/NacosRestfulProvider.java
+++ nacos-restful-provider/src/main/java/cn/isjinhao/sc/provider/NacosRestfulProvider.java
@@ -1,19 +1,52 @@
 package cn.isjinhao.sc.provider;
 
+import cn.addenda.businesseasy.propertyrefresh.PropertyRefresh;
+import cn.addenda.businesseasy.propertyrefresh.PropertyRefreshHolder;
+import cn.addenda.businesseasy.propertyrefresh.PropertyRefreshListener;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author ISJINHAO
  * @Date 2021/4/18 19:04
  */
 @RestController
-public class NacosRestfulProvider {
+@Transactional(rollbackFor = Exception.class)
+public class NacosRestfulProvider implements InitializingBean, PropertyRefreshListener {
+
+    @Value("#{'${propertyrefreshtest}'.split(',')}")
+    @PropertyRefresh
+    private List<String> addressList;
 
     // 暴露RESTful接口
     @GetMapping(value = "/nacosRestfulProvider")
     public String service() {
         return "nacosRestfulProvider is called!";
+    }
+
+    ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            System.out.println(addressList);
+        }, 10, 10, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void doPropertyRefresh(PropertyRefreshHolder propertyRefreshHolder) {
+        System.out.println(propertyRefreshHolder);
+        if (propertyRefreshHolder.getFieldName().equals("addressList")) {
+            addressList = (List<String>) propertyRefreshHolder.getNewValue();
+        }
     }
 
 }
